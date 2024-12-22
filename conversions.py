7e247725112a9d21c19e2f58010fb88ccfc96f44
bmblankenship <brandonmblankenship1991@gmail.com>
2024-12-22 12:07:49 -0500

    adjustments
    deleted conversions file, completed baseline functionality

--- conversions.py
+++
@@ -1,8 +0,0 @@
-def miles_to_km():
-    return 1.60934
-
-def miles_to_feet():
-    return 5280
-
-def miles_to_meters():
-    return 1609.34
--- main.py
+++ main.py
@@ -1,12 +1,49 @@
 import tkinter as tk
-from conversions import *
 
-# Create the main window
-root = tk.Tk()
-root.title("Unit Converter")
-root.geometry("600x400")
+def main():
+    # Create the main window
+    root = tk.Tk()
+    root.title("Unit Converter")
+    root.geometry("600x400")
 
-def convert():
+    # string variables
+    input_var = tk.StringVar()
+    output_var = tk.StringVar()
+
+    # Create Input fields
+    input_label = tk.Label(root, text="Input", font=('calibre', 10, 'bold'))
+    input_entry = tk.Entry(root, textvariable=input_var, font=('calibre', 10, 'normal'))
+    input_options = ["Miles", "Kilometers", "Feet", "Meters"]
+    input_value = tk.StringVar(root)
+    input_value.set("Selection an Option")
+    input_menu = tk.OptionMenu(root, input_value, *input_options)
+
+    # Create Output fields
+    output_label = tk.Label(root, text="Output", font=('calibre', 10, 'bold'))
+    output_entry = tk.Entry(root, textvariable=output_var, font=('calibre', 10, 'normal'))
+    output_options = ["Miles", "Kilometers", "Feet", "Meters"]
+    output_value = tk.StringVar(root)
+    output_value.set("Selection an Option")
+    output_menu = tk.OptionMenu(root, output_value, *output_options)
+
+    # Convert Button
+    convert_btn = tk.Button(root, text='Convert', command=lambda: convert(input_var, input_value, output_value, output_var))
+
+    # Place widgets
+    input_label.grid(row=1, column=0)
+    input_entry.grid(row=2, column=0)
+    input_menu.grid(row=3, column=0)
+
+    output_label.grid(row=1, column=2)
+    output_entry.grid(row=2, column=2)
+    output_menu.grid(row=3, column=2)
+
+    convert_btn.grid(row=4, column=1)
+
+    # Start the GUI event Loop
+    root.mainloop()
+
+def convert(input_var, input_value, output_value, output_var):
     input_field = float(input_var.get())
 
     input_unit = input_value.get()
@@ -21,54 +58,48 @@ def calc_convert(in_unit, out_unit, val):
         match in_unit:
             case "Miles":
                 return convert_miles(out_unit, val)
+            case "Kilometers":
+                return convert_kilometers(out_unit, val)
+            case "Feet":
+                return convert_feet(out_unit, val)
+            case "Meters":
+                return convert_meters(out_unit, val)
 
 def convert_miles(out_unit, val):
     match out_unit:
         case "Kilometers":
-            return val * miles_to_km()
+            return round(val * 1.60934, 2)
         case "Feet":
-            return val * miles_to_feet()
+            return round(val * 5280, 2)
         case "Meters":
-            return val * miles_to_meters()
+            return round(val * 1609.34, 2)
 
-# string variables
-input_var = tk.StringVar()
-output_var = tk.StringVar()
+def convert_kilometers(out_unit, val):
+    match out_unit:
+        case "Miles":
+            return round(val * 0.621371, 2)
+        case "Feet":
+            return round(val * 3280.84, 2)
+        case "Meters":
+            return round(val * 1000, 2)
 
-# Create Miles fields
-input_label = tk.Label(root, text = "Input", font=('calibre', 10, 'bold'))
-input_entry = tk.Entry(root,textvariable = input_var, font=('calibre', 10, 'normal'))
+def convert_feet(out_unit, val):
+    match out_unit:
+        case "Miles":
+            return round(val * 0.000189394, 2)
+        case "Kilometers":
+            return round(val * 0.0003048, 2)
+        case "Meters":
+            return round(val * 0.3048, 2)
 
-# Create Inches fields
-output_label = tk.Label(root, text = "Output", font=('calibre', 10, 'bold'))
-output_entry = tk.Entry(root,textvariable = output_var, font=('calibre', 10, 'normal'))
+def convert_meters(out_unit, val):
+    match out_unit:
+        case "Miles":
+            return round(val * 0.000621371, 2)
+        case "Kilometers":
+            return round(val * 0.001, 2)
+        case "Feet":
+            return round(val * 3.28084, 2)
 
-# Convert Button
-convert_btn = tk.Button(root, text = 'Convert', command = convert)
-
-# Input unit
-input_options = ["Miles", "Kilometers", "Feet", "Meters"]
-input_value = tk.StringVar(root)
-input_value.set("Selection an Option")
-input_menu = tk.OptionMenu(root, input_value, *input_options)
-
-# Output unit
-output_options = ["Miles", "Kilometers", "Feet", "Meters"]
-output_value = tk.StringVar(root)
-output_value.set("Selection an Option")
-output_menu = tk.OptionMenu(root, output_value, *output_options)
-
-
-# Place widgets
-input_label.grid(row=1, column=0)
-input_entry.grid(row=2, column=0)
-input_menu.grid(row=3, column=0)
-
-output_label.grid(row=1, column=2)
-output_entry.grid(row=2, column=2)
-output_menu.grid(row=3, column=2)
-
-convert_btn.grid(row=4, column=1)
-
-# Start the GUI event Loop
-root.mainloop()
+if __name__ == '__main__':
+    main()
