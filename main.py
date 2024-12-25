a75d1653d24fd82a0397dc7b6725645097695336
bmblankenship <brandonmblankenship1991@gmail.com>
2024-12-24 19:08:00 -0500

    Update main.py
    restructured into a class

--- main.py
+++ main.py
@@ -1,126 +1,118 @@
 import tkinter as tk
 
-def main():
+class UnitConverter:
 
-    # Create the main window
-    root = tk.Tk()
-    root.title("Unit Converter")
-    window_width = 600
-    window_height = 400
-    root.geometry("{}x{}".format(window_width,window_height))
+    def __init__(self):
+        # Create Main Window
+        self.window_width = 600
+        self.window_height = 400
+        self.root = tk.Tk()
+        self.root.title("Unit Converter")
+        self.root.geometry("{}x{}".format(self.window_width, self.window_height))
 
-    # string variables
-    input_var = tk.StringVar()
-    output_var = tk.StringVar()
+        # String Variables
+        self.input_var = tk.StringVar()
+        self.output_var = tk.StringVar()
 
-    # conversion type selection
-    type_value = tk.StringVar(root)
-    type_options = ["Length", "Volume", "Weight"]
-    type_value.set("Select Conversion Type")
-    type_menu = tk.OptionMenu(root, type_value, *type_options)
+        # Create Fonts
+        self.bold_font = ('calibre', 10, 'bold')
+        self.normal_font = ('calibre', 10, 'normal')
 
-    # Create Input fields
-    input_label = tk.Label(root, text="Input", font=('calibre', 10, 'bold'))
-    input_entry = tk.Entry(root, textvariable=input_var, font=('calibre', 10, 'normal'))
-    input_options = unit_list(type_value)
-    input_value = tk.StringVar(root)
-    input_value.set("Select Input Unit")
-    input_menu = tk.OptionMenu(root, input_value, *input_options)
+        # Create Input Labels
+        self.input_label = tk.Label(self.root, text="Input", font=self.bold_font)
+        self.input_entry = tk.Entry(self.root, textvariable=self.input_var, font=self.normal_font)
+        self.input_options = unit_list()
+        self.input_value = tk.StringVar(self.root)
+        self.input_value.set("Select Input Unit")
+        self.input_menu = tk.OptionMenu(self.root, self.input_value, *self.input_options)
 
-    # Create Output fields
-    output_label = tk.Label(root, text="Output", font=('calibre', 10, 'bold'))
-    output_entry = tk.Entry(root, textvariable=output_var, font=('calibre', 10, 'normal'))
-    output_options = unit_list(type_value)
-    output_value = tk.StringVar(root)
-    output_value.set("Select Output Unit")
-    output_menu = tk.OptionMenu(root, output_value, *output_options)
+        # Create Output fields
+        self.output_label = tk.Label(self.root, text="Output", font=self.bold_font)
+        self.output_entry = tk.Entry(self.root, textvariable=self.output_var, font=self.normal_font)
+        self.output_options = unit_list()
+        self.output_value = tk.StringVar(self.root)
+        self.output_value.set("Select Output Unit")
+        self.output_menu = tk.OptionMenu(self.root, self.output_value, *self.output_options)
 
-    # Convert Button
-    convert_btn = tk.Button(root, text='Convert', command=lambda: convert(input_var, input_value, output_value, output_var))
+        # Convert Button
+        self.convert_btn = tk.Button(self.root, text='Convert', command= self.convert)
 
-    # Place widgets
-    type_menu.place(relx=0.50, rely=0.2, anchor="n")
+    def start_converter(self):
+        self.place_widgets()
+        self.root.mainloop()
 
-    input_label.place(relx=0.25 , rely=0.4, anchor="n")
-    input_entry.place(relx=0.25 , rely=0.5, anchor="n")
-    input_menu.place(relx=0.25 , rely=0.6, anchor="n")
+    def place_widgets(self):
+        # Place widgets
 
-    output_label.place(relx=0.75 , rely=0.4, anchor="n")
-    output_entry.place(relx=0.75 , rely=0.5, anchor="n")
-    output_menu.place(relx=0.75 , rely=0.6, anchor="n")
+        self.input_label.place(relx=0.25, rely=0.4, anchor="n")
+        self.input_entry.place(relx=0.25, rely=0.5, anchor="n")
+        self.input_menu.place(relx=0.25, rely=0.6, anchor="n")
 
-    convert_btn.place(relx=0.5, rely=0.9, anchor="n")
+        self.output_label.place(relx=0.75, rely=0.4, anchor="n")
+        self.output_entry.place(relx=0.75, rely=0.5, anchor="n")
+        self.output_menu.place(relx=0.75, rely=0.6, anchor="n")
 
-    # Start the GUI event Loop
-    root.mainloop()
+        self.convert_btn.place(relx=0.5, rely=0.9, anchor="n")
 
-def unit_list(type_value):
-    if type_value == "Length":
-        return ["Miles", "Kilometers", "Feet", "Meters"]
-    elif type_value == "Volume":
-        return ["Quarts", "Liters", "Gallons", "Milliliters"]
-    elif type_value == "Weight":
-        return ["Pounds", "Kilograms"]
-    else:
-        return["error"]
+    def convert(self):
+        input_field = float(self.input_var.get())
+        self.output_var.set(str(self.calc_convert(input_field)))
 
-def convert(input_var, input_value, output_value, output_var):
-    input_field = float(input_var.get())
+    def calc_convert(self, val):
+        if self.input_value == self.output_value:
+            return val
+        else:
+            match self.input_value:
+                case "Miles":
+                    return self.convert_miles(val)
+                case "Kilometers":
+                    return self.convert_kilometers(val)
+                case "Feet":
+                    return self.convert_feet(val)
+                case "Meters":
+                    return self.convert_meters(val)
 
-    input_unit = input_value.get()
-    output_unit = output_value.get()
-
-    output_var.set(str(calc_convert(input_unit, output_unit, input_field)))
-
-def calc_convert(in_unit, out_unit, val):
-    if in_unit == out_unit:
-        return val
-    else:
-        match in_unit:
-            case "Miles":
-                return convert_miles(out_unit, val)
+    def convert_miles(self, val):
+        match self.output_value:
             case "Kilometers":
-                return convert_kilometers(out_unit, val)
+                return round(val * 1.60934, 2)
             case "Feet":
-                return convert_feet(out_unit, val)
+                return round(val * 5280, 2)
             case "Meters":
-                return convert_meters(out_unit, val)
+                return round(val * 1609.34, 2)
 
-def convert_miles(out_unit, val):
-    match out_unit:
-        case "Kilometers":
-            return round(val * 1.60934, 2)
-        case "Feet":
-            return round(val * 5280, 2)
-        case "Meters":
-            return round(val * 1609.34, 2)
+    def convert_kilometers(self, val):
+        match self.output_value:
+            case "Miles":
+                return round(val * 0.621371, 2)
+            case "Feet":
+                return round(val * 3280.84, 2)
+            case "Meters":
+                return round(val * 1000, 2)
 
-def convert_kilometers(out_unit, val):
-    match out_unit:
-        case "Miles":
-            return round(val * 0.621371, 2)
-        case "Feet":
-            return round(val * 3280.84, 2)
-        case "Meters":
-            return round(val * 1000, 2)
+    def convert_feet(self, val):
+        match self.output_value:
+            case "Miles":
+                return round(val * 0.000189394, 2)
+            case "Kilometers":
+                return round(val * 0.0003048, 2)
+            case "Meters":
+                return round(val * 0.3048, 2)
 
-def convert_feet(out_unit, val):
-    match out_unit:
-        case "Miles":
-            return round(val * 0.000189394, 2)
-        case "Kilometers":
-            return round(val * 0.0003048, 2)
-        case "Meters":
-            return round(val * 0.3048, 2)
+    def convert_meters(self, val):
+        match self.output_value:
+            case "Miles":
+                return round(val * 0.000621371, 2)
+            case "Kilometers":
+                return round(val * 0.001, 2)
+            case "Feet":
+                return round(val * 3.28084, 2)
 
-def convert_meters(out_unit, val):
-    match out_unit:
-        case "Miles":
-            return round(val * 0.000621371, 2)
-        case "Kilometers":
-            return round(val * 0.001, 2)
-        case "Feet":
-            return round(val * 3.28084, 2)
+def unit_list():
+    return["Miles", "Kilometers", "Feet", "Meters",
+        "Quarts", "Liters", "Gallons", "Milliliters",
+        "Pounds", "Kilograms"]
 
 if __name__ == '__main__':
-    main()
+    uc = UnitConverter()
+    uc.start_converter()
