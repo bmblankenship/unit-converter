59b437ef7ab9f6be51530889395f31eeee3c985c
bmblankenship <brandonmblankenship1991@gmail.com>
2024-12-24 20:59:07 -0500

    refactoring
    adding settings file to save some visual clutter in main class. Renamed main file

---
+++ settings.py
@@ -0,0 +1,17 @@
+class Settings:
+    """
+    A class to store configuration settings for unit_converter.py
+    """
+    def __init__(self):
+        # Window Settings
+        self.window_width = 600
+        self.window_height = 400
+
+        # Font Settings
+        self.bold_font = ('calibre', 10, 'bold')
+        self.normal_font = ('calibre', 10, 'normal')
+
+        # Menu Settings
+        self.length_options = {"Miles", "Kilometers", "Feet", "Meters"}
+        self.volume_options = {"Quarts", "Liters", "Gallons", "Milliliters"}
+        self.weight_options = {"Pounds", "Kilograms"}
--- unit_converter.py
+++ unit_converter.py
@@ -1,34 +1,53 @@
 import tkinter as tk
+from settings import Settings
 
 class UnitConverter:
 
     def __init__(self):
         # Create Main Window
-        self.window_width = 600
-        self.window_height = 400
+        self.set = Settings()
         self.root = tk.Tk()
         self.root.title("Unit Converter")
-        self.root.geometry("{}x{}".format(self.window_width, self.window_height))
+        self.root.geometry("{}x{}".format(self.set.window_width, self.set.window_height))
 
         # String Variables
         self.input_var = tk.StringVar()
         self.output_var = tk.StringVar()
+        self.type_var = tk.StringVar(self.root, "0")
 
-        # Create Fonts
-        self.bold_font = ('calibre', 10, 'bold')
-        self.normal_font = ('calibre', 10, 'normal')
+        # Type Selection
+        self.type_length = tk.Radiobutton(self.root, text="Length", variable=self.type_var, value = "1", command=self.length_selected)
+        self.type_volume = tk.Radiobutton(self.root, text="Volume", variable=self.type_var, value = "2", command=self.volume_selected)
+        self.type_weight = tk.Radiobutton(self.root, text="Weight", variable=self.type_var, value = "3", command=self.weight_selected)
 
-        # Create Input Labels
-        self.input_label = tk.Label(self.root, text="Input", font=self.bold_font)
-        self.input_entry = tk.Entry(self.root, textvariable=self.input_var, font=self.normal_font)
+        # Create Inputs
+        self.input_label = tk.Label(self.root, text="Input", font=self.set.bold_font)
+        self.input_entry = tk.Entry(self.root, textvariable=self.input_var, font=self.set.normal_font)
+
+        self.input_length_value = tk.StringVar(self.root)
+        self.input_length_value.set("Length")
+        self.input_length_menu = tk.OptionMenu(self.root, self.input_length_value, *self.set.length_options)
+        self.input_length_menu.config(state="disabled")
+
+        self.input_volume_value = tk.StringVar(self.root)
+        self.input_volume_value.set("Volume")
+        self.input_volume_menu = tk.OptionMenu(self.root, self.input_volume_value, *self.set.volume_options)
+        self.input_volume_menu.config(state="disabled")
+
+        self.input_weight_value = tk.StringVar(self.root)
+        self.input_weight_value.set("Weight")
+        self.input_weight_menu = tk.OptionMenu(self.root, self.input_weight_value, *self.set.weight_options)
+        self.input_weight_menu.config(state="disabled")
+
+        # to be deleted
         self.input_options = unit_list()
         self.input_value = tk.StringVar(self.root)
         self.input_value.set("Select Input Unit")
         self.input_menu = tk.OptionMenu(self.root, self.input_value, *self.input_options)
 
-        # Create Output fields
-        self.output_label = tk.Label(self.root, text="Output", font=self.bold_font)
-        self.output_entry = tk.Entry(self.root, textvariable=self.output_var, font=self.normal_font)
+        # Create Outputs
+        self.output_label = tk.Label(self.root, text="Output", font=self.set.bold_font)
+        self.output_entry = tk.Entry(self.root, textvariable=self.output_var, font=self.set.normal_font)
         self.output_options = unit_list()
         self.output_value = tk.StringVar(self.root)
         self.output_value.set("Select Output Unit")
@@ -37,19 +56,41 @@ class UnitConverter:
         # Convert Button
         self.convert_btn = tk.Button(self.root, text='Convert', command= self.convert)
 
+    def length_selected(self):
+        self.input_length_menu.config(state="normal")
+        self.input_volume_menu.config(state="disabled")
+        self.input_weight_menu.config(state="disabled")
+
+    def volume_selected(self):
+        self.input_length_menu.config(state="disabled")
+        self.input_volume_menu.config(state="normal")
+        self.input_weight_menu.config(state="disabled")
+
+    def weight_selected(self):
+        self.input_length_menu.config(state="disabled")
+        self.input_volume_menu.config(state="disabled")
+        self.input_weight_menu.config(state="normal")
+
     def start_converter(self):
         self.place_widgets()
         self.root.mainloop()
 
     def place_widgets(self):
         # Place widgets
+        self.type_length.place(relx=0.25, rely=0.1, anchor="n")
+        self.type_volume.place(relx=0.5, rely=0.1, anchor="n")
+        self.type_weight.place(relx=0.75, rely=0.1, anchor="n")
 
-        self.input_label.place(relx=0.25, rely=0.4, anchor="n")
-        self.input_entry.place(relx=0.25, rely=0.5, anchor="n")
+        self.input_label.place(relx=0.25, rely=0.2, anchor="n")
+        self.input_entry.place(relx=0.25, rely=0.3, anchor="n")
         self.input_menu.place(relx=0.25, rely=0.6, anchor="n")
 
-        self.output_label.place(relx=0.75, rely=0.4, anchor="n")
-        self.output_entry.place(relx=0.75, rely=0.5, anchor="n")
+        self.input_length_menu.place(relx=0.25, rely=0.7, anchor="n")
+        self.input_volume_menu.place(relx=0.25, rely=0.8, anchor="n")
+        self.input_weight_menu.place(relx=0.25, rely=0.9, anchor="n")
+
+        self.output_label.place(relx=0.75, rely=0.2, anchor="n")
+        self.output_entry.place(relx=0.75, rely=0.3, anchor="n")
         self.output_menu.place(relx=0.75, rely=0.6, anchor="n")
 
         self.convert_btn.place(relx=0.5, rely=0.9, anchor="n")
